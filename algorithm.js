9c77b00a74d92fbaaa3991c43232a5d97845f093
gkswn45 <73797608+gkswn45@users.noreply.github.com>
2021-07-01 21:59:02 +0900

    Update algorithm.js

--- algorithm.js
+++ algorithm.js
@@ -12,14 +12,14 @@
 // }
 // console.log(solution('good'));
 
-//substr 
+//substr
 //substring 차이점은
 // substr는 splice 처럼 index를 지정해서 몇개를 뽑아낼지 정하는거고
 // substring은 index부터 다음번 인덱스 전까지를 뽑아내는거다.
 
 //s.indexOf('k', 1) // 1번 index이후로 찾아라는것
 // 없으면 처음부터 하는것
-// s.idxOf(x) === -1 false 존재하지않으면 
+// s.idxOf(x) === -1 false 존재하지않으면
 
 // function solution(s) {
 //     let answer;
@@ -87,7 +87,7 @@
 // }
 
 // let a=[2, 3, 3, 1, 3];
-// let b=[1, 1, 2, 2, 3]; //1가위 2바위 3보 
+// let b=[1, 1, 2, 2, 3]; //1가위 2바위 3보
 // console.log(solution(a, b));
 
 // let a=[2, 3, 3, 1, 3];
@@ -114,7 +114,7 @@
 // let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
 // console.log(solution(10, arr));
 
-// 등수구하기 
+// 등수구하기
 
 // function solution(N, arr) {
 //     let answer = Array(N).fill(1);
@@ -136,7 +136,7 @@
 // // 6.격자판 최대합
 
 
-// 내가 푼 방식 
+// 내가 푼 방식
 
 // function solution(N, arr) {
 //     let answer = 0;
@@ -164,7 +164,7 @@
 // }
 
 // let N = 6;
-// let list=[[7, 5, 3, 2, 2, 3], 
+// let list=[[7, 5, 3, 2, 2, 3],
 //          [2, 3, 7, 1, 6, 1],
 //          [3, 7, 2, 5, 3, 4],
 //          [4, 4, 3, 6, 4, 1],
@@ -190,7 +190,7 @@
 //                 if(nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
 //                     flag = 0;
 //                     break;
-//                 } 
+//                 }
 //             }
 //             if(flag) answer++;
 //         }
@@ -198,7 +198,7 @@
 //     return answer;
 // }
 
-// let list=[[5, 3, 7, 2, 3], 
+// let list=[[5, 3, 7, 2, 3],
 //           [3, 7, 1, 6, 1],
 //           [7, 2, 5, 3, 4],
 //           [4, 3, 6, 4, 1],
@@ -267,7 +267,7 @@
 //     console.log(word, word2);
 //     return answer;
 // }
-            
+
 // let str="found7, time: study; Yduts; emit, 7Dnuof";
 // console.log(solution(str));
 
@@ -313,7 +313,7 @@
 //     let answer = [];
 //     let P = 1000;
 //     for(let i = 0; i < s.length; i++) {
-//         if(s[i] === t) { 
+//         if(s[i] === t) {
 //             P = 0;
 //             answer.push(P);
 //         } else {
@@ -328,7 +328,7 @@
 //         }
 //         else {
 //             P++
-//             answer[i] = Math.min(answer[i], P); 
+//             answer[i] = Math.min(answer[i], P);
 //         }
 //     }
 //     return answer;
@@ -460,7 +460,7 @@
 // }
 
 
-// 숫자로 푼방법 
+// 숫자로 푼방법
 
 // function solution(n, arr){
 //     let answer, max=Number.MIN_SAFE_INTEGER;
@@ -518,7 +518,7 @@
 // function isPrime(num){
 //     let answer = num.map((ele) => String(ele));
 //     let list = [];
-    
+
 //     for(let x of answer) {
 //         let count = 0;
 //         let word = parseInt(x.split('').reverse().join(''));
@@ -530,7 +530,7 @@
 //         if(count === 2) {
 //             list.push(word);
 //         }
-//         count = 0; 
+//         count = 0;
 //     }
 //     return list;
 // }
@@ -541,33 +541,298 @@
 
 /// 4 - 3 멘토링 시스템
 
-function solution(test) {
-    let answer = 0, tmp = []; // 결과값 , 경우의수의 상세정보
-    let N = test[0].length; // 학생수
-    let M = test.length; // 시험획수
+// function solution(test) {
+//     let answer = 0, tmp = []; // 결과값 , 경우의수의 상세정보
+//     let N = test[0].length; // 학생수
+//     let M = test.length; // 시험획수
 
-    for(let i = 1; i <= N; i++) { // 멘토후보 i 학생
-        for(let j = 1; j <= N; j++) { // 멘티후보 j 학생
-            if(i !== j) { // 둘이같을경우는 존재하지 않기때문.
-                let count = 0; // k번째 시험때 멘토로 가정한 i와 멘티로 가정한 j의 관계가 성립하는경우
-                for(let k = 0; k < M; k++) { // M을 쓰는 이유는 k의경우는 k번째 시험을 나타내기위해 사용하기 때문이다.
-                    let pi = pj = 0;
-                    for(let s = 0; s < N; s++) { // 여기서 s 는 곧 k번째시험에 s 번째를 탐색하여 i학생의 등수와 j 학생의 등수를 알아내기 위해 사용한다.
-                        if(test[k][s] === i) pi = s; // i번째 학생의 등수는 index s 번째이다 ex) 3번째에 위치해 있으면 3등이다.
-                        if(test[k][s] === j) pj = s; // 마찬가지.
-                    }
-                    if(pi < pj) count++; // k번째 시험때 i 학생이 j 학생보다 등수가 높을경우 count++해준다.
-                }
-                if(count === M) { // 멘토멘티로 가정한 관계가 모든시험에서 성립될경우 결과값에 ++ 해주고 tmp에 push해준다.
-                    answer++;
-                    tmp.push([i, j]);
-                }
-            }
-        }
-    }
-    // console.log(tmp, answer); 확인을위한 콘솔부분
-    return answer;
-}
+//     for(let i = 1; i <= N; i++) { // 멘토후보 i 학생
+//         for(let j = 1; j <= N; j++) { // 멘티후보 j 학생
+//             if(i !== j) { // 둘이같을경우는 존재하지 않기때문.
+//                 let count = 0; // k번째 시험때 멘토로 가정한 i와 멘티로 가정한 j의 관계가 성립하는경우
+//                 for(let k = 0; k < M; k++) { // M을 쓰는 이유는 k의경우는 k번째 시험을 나타내기위해 사용하기 때문이다.
+//                     let pi = pj = 0;
+//                     for(let s = 0; s < N; s++) { // 여기서 s 는 곧 k번째시험에 s 번째를 탐색하여 i학생의 등수와 j 학생의 등수를 알아내기 위해 사용한다.
+//                         if(test[k][s] === i) pi = s; // i번째 학생의 등수는 index s 번째이다 ex) 3번째에 위치해 있으면 3등이다.
+//                         if(test[k][s] === j) pj = s; // 마찬가지.
+//                     }
+//                     if(pi < pj) count++; // k번째 시험때 i 학생이 j 학생보다 등수가 높을경우 count++해준다.
+//                 }
+//                 if(count === M) { // 멘토멘티로 가정한 관계가 모든시험에서 성립될경우 결과값에 ++ 해주고 tmp에 push해준다.
+//                     answer++;
+//                     tmp.push([i, j]);
+//                 }
+//             }
+//         }
+//     }
+//     // console.log(tmp, answer); 확인을위한 콘솔부분
+//     return answer;
+// }
 
-let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
-console.log(solution(arr));
+// let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
+// console.log(solution(arr));
+
+//// 4 - 4 졸업선물
+
+
+// function solution(m, product) {
+//     let answer = 0;
+//     let n = product.length;
+//     // 값이 싼 가격부터 오름차순으로 정렬해준다.
+//     product.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1])); // b 가 앞에숫자 a 가 뒤의 숫자이다.
+//     // 그렇다면 여기서는 (2 + 2) - (6 + 6) = - 1 보다 작기 때문에 false이다.
+//     // 그럼 a에 속해있는 [6, 6] 을 뒤로 보내준다 [2, 2]는앞으로 오게되고
+//     // 6, 6 과 4,3을비교한다 이런식으로 계쏙 진행 되서 오름차순으로 결국 정렬된다.
+
+//     for(let i = 0; i < n; i++) {
+//         let money = m - (product[i][0] / 2 + (product[i][1]));
+//         let cnt = 1;
+//         for(let j = 0; j < n; j++) {
+//             let money2 = product[j][0] + product[j][1];
+//             if(j !== i & money < money2) break;
+//             if(j !== i & money >= money2) {
+//                 money -= money2;
+//                 cnt++
+//             }
+//         }
+//         answer = Math.max(answer, cnt);
+//     }
+//     return answer;
+// }
+
+/// 4 - 5 K번째 큰수
+
+// function solution(k, list) {
+//     let answer = [];
+//     let n = list.length;
+//     for(let i = 0; i < n - 2; i++) {
+//         for(let j = i + 1; j < n - 1; j++) {
+//             for(let z = j + 1; z < n; z++) {
+//                 answer.push(list[i] + list[j] + list[z]);
+//             }
+//         }
+//     }
+//     answer.sort((a, b) => b - a);
+//     let newanswer = [...new Set(answer)];
+
+
+//     return newanswer[k - 1];
+// }
+// // 10 3
+
+// let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+// console.log(solution(3, arr));
+
+
+// 5 - 1 배열 붙이고 오름차순으로 정렬하기
+
+// function solution(arr1, arr2) {
+//     let answer = [];
+//     // concat 메서드 사용방법
+//     // let answer = [];
+//     // answer = answer.concat(arr1, arr2).sort((a, b) => a - b);
+
+//     // ... 연산자 spread operator 사용방법
+//     // let answer = [
+//     //     ...arr1,
+//     //     ...arr2
+//     // ];
+
+//     // push(... array)함수로 배열 합치기
+// console.log(arr1.push(...arr2));
+// return answer;
+// }
+
+// let a=[1, 3, 5];
+// let b=[2, 3, 6, 7, 9];
+// console.log(solution(a, b));
+
+// 강의에서 알려주는 방법
+// function solution(arr1, arr2){
+//     let answer = [];
+//     let n = arr2.length;
+//     let m = arr2.length;
+//     let p1 = p2 = 0;
+//     while(p1< n && p2< m) { 
+//         if(arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]); // 뒤에 ++을하면 후칙연산으로하고
+//             //즉 지금값이 1이면 1을 넣고 ++해서 2가된다.
+//             else answer.push(arr2[p2++]);
+//     }
+//     while(p1 < n) answer.push(arr1[p1++]);
+//     while(p2 < m) answer.push(arr2[p2++]);
+//     return answer;
+// }
+// let a=[1, 3, 5];
+// let b=[2, 3, 6, 7, 9];
+// console.log(solution(a, b));
+
+// 5 - 2 공통원소 구하기
+
+// // 내가 푼 방식
+
+// function solution(arr1, arr2){
+//     let answer=[];
+//     arr1.sort((a, b) => a - b);
+//     arr2.sort((a, b) => a - b);
+//     console.log(arr1, arr2);
+//     for(let x of arr1) {
+//         for(let i = 0; i < arr1.length; i++) {
+//             if(x === arr2[i]) {
+//                 answer.push(x);
+//                 break;
+//             }
+//         }
+//     }
+//     return answer;
+// }
+
+// let a=[1, 3, 9, 5, 2];
+// let b=[3, 2, 5, 7, 8];
+// console.log(solution(a, b));
+
+// 강사님이 푼 방식 
+
+// function solution(arr1, arr2) {
+//     let answer = [];
+//     let p1 = p2 = 0;
+//     while(p1 < arr1.length && p2 < arr2.length) {
+//         if(arr1[p1] === arr2[p2]) {
+//             answer.push(arr1[p1++])
+//             p2++;
+//         } else if (arr1[p1] < arr2[p2]) p1++;
+//         else p2++;
+//     }
+//     return answer;
+// }
+
+// let a=[1, 3, 9, 5, 2];
+// let b=[3, 2, 5, 7, 8];
+// console.log(solution(a, b));
+
+/// 5 - 3 연속 부분수열 1
+//내가 분 방법
+// function solution(m, arr){
+//     let answer = 0, len = arr.length, sum = 0, int = 0, i = 0;
+//     while(i < len + 1) {
+//         if(sum === m) {
+//             answer++;
+//             int++;
+//             i = int;
+//             sum = 0;
+//         } else if(sum > m) {
+//             int++;
+//             i = int;
+//             sum = 0;
+//         } else {
+//             sum += arr[i];
+//             i++;
+//         }
+//     }
+//     return answer;
+// }
+
+
+// 강사님이 푼 방법
+// function solution(m, arr) {
+//     let answer = 0, lt = 0, sum = 0;
+//     for(let rt = 0; rt < arr.length; rt++) {
+//         sum += arr[rt];
+//         if(sum === m) answer++;
+//         while(sum >= m ) {
+//             sum -= arr[lt++];
+//             if(sum === m) {
+//                 answer++;
+//                 console.log(lt, rt);
+//         }
+//     }
+// }
+//     return answer;
+// }
+
+
+// let a=[1, 2, 1, 3, 1, 1, 1, 2];
+// console.log(solution(6, a));
+
+// 1 2 1 3 7?
+// while문들어가서 7 - arr[0++] 1 = 6
+// if문확인 값이 같으므로 answer ++해주고 여전히 sum은 m 이상이기때문에
+// 6 - arr[1] = 6 - 2 = 4 이므로 while문 탈출;
+// 그럼 for문으로 돌아가서 rt ++ 해주고 그럼 rt 는 1
+// 4 + arr[1] = 4 + 2 = 6 그럼 answer++ 해주고
+// while문은 참 이므로 6 = arr[2] = 6 - 1 =5 이므로 while문 탈출
+// for문 rt+= sum - arr[rt]  = 5+ arr[rt] arr[rtt] = 1  rmfja 6이된다 그럼 또 while문
+
+// 그럼 맘지막은 어떻게될까 3111 에서 3을 빼면 111 이고 뒤에 2가밖에 안나맛다
+// 그럼  1 + 1 + 1  + 2 = 5 가되고 while문을 들어가지않으므ㅗㄹ rt는 끝나게 된다.a
+// 근데만일크다? 그럼 Ehwhile 들어가는거지 시간복잡도 log(n) 랙무
+
+/// 5 - 4 연속 부분수열2 연속부분수열부분이 어렵다..
+
+// function solution(m, arr){
+//     let answer = 0 , sum = 0, lt = 0;
+//     for(let rt = 0; rt < arr.length; rt++) {
+//         sum += arr[rt];
+//         while(sum > m) {
+//             sum -= arr[lt++]; // 6이된다. lt는 2가되고 6 - 3을해서 sum 은 3이된다.
+//         }
+//         answer += (rt - lt + 1);
+//         console.log(lt, rt);
+//     }
+
+//     return answer;
+// }
+
+// let a=[1, 3, 1, 2, 3];
+// console.log(solution(5, a));
+
+//--------------------------------------------------------------------------------------------
+// /// 여기서 핵심은 1, 3, 1, 2, 3 일때 
+// lt, rt로 해주는데
+// lt는 왼쪽숫자이고 rt는 오른쪽숫자라고 생각하면 편하다.
+
+// 즉 rt를 증가시켜가면서 주어진 값 보다 작을때 ++ 해주고 
+// 만일 rt가 주어진값보다 커지면 rt에서 lt를 마이너스 해가면서 주어진값보다 작이지면 ++ 해주고
+// 다시 rt를 증가시켜준다. 이거반복
+
+// 5 - 5 최대매출 
+// 내가한 방법
+// function solution(k, arr) {
+//     let answer = 0 , sum = 0;
+//     for(let i = 0; i <= arr.length - k; i++) {
+//         sum =  arr[i] + arr[i + 1] + arr[i + 2];
+//         if(sum > answer) answer = sum;
+//         console.log(sum);
+//     }
+//     return answer;
+// }
+
+// let a=[12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+// console.log(solution(3, a));
+
+// 강사님이 한방법 
+
+// function solution(k, arr) {
+//     let answer, sum = 0;
+//     for(let i = 0; i < k; i++) {
+//         sum += arr[i];
+//     }
+//     answer = sum;
+//     for(let i = k; i < arr.length; i++) {
+//         sum += (arr[i] - arr[i - k]);
+//         answer = Math.max(answer, sum);
+//     }
+//     return answer;
+// }
+// let a=[12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+// console.log(solution(3, a));
+
+
+// 시간측정하기
+// var sum = 0;
+
+// console.time("calculatingTime")
+// console.log(1);
+
+// console.timeEnd('calculatingTime');
+// // calculatingTime: 73.687255859375 ms
+
