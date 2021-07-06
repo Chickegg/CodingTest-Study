b8486c872f4d387c234e309891b001b9414a1e26
unknown <73797608+Chickegg@users.noreply.github.com>
2021-07-06 20:26:55 +0900

    Update 프로그래머스.js
    LV2 입성

--- 프로그래머스.js
+++ 프로그래머스.js
@@ -1,4 +1,6 @@
-// LV1 2개뽑아서 더하기
+// /// ----------------------------------- L V 1 -------------------------------------------// 
+
+// (2개뽑아서 더하기)
 
 // 내가푼방법
 // function solution(numbers) {
@@ -54,3 +56,51 @@
 // const n = 12345;
 // console.log(solution(n));
 
+
+
+
+/// ----------------------------------- L V 1 -------------------------------------------// 
+
+// (행렬의 곱셈)
+// function solution(arr1, arr2) {
+//     let answer = [];
+
+//     for(let i = 0; i < arr1.length; i++) {
+//         answer.push([]);
+//         for(let j = 0; j < arr2[0].length; j++) {
+//             let sum = 0;
+//             for(let n = 0; n < arr2.length; n++) {
+//                 sum += arr1[i][n] * arr2[n][j];
+//             }
+//             answer[i].push(sum);
+//         }
+//     }
+//     return answer;
+// }
+
+// let a = [[1, 4], [3, 2], [4, 1]];
+// let b = [[3, 3], [3, 3]];
+// console.log(solution(a, b));
+
+// (N개의 최소공배수)
+
+// function solution(arr) {
+//     let LastNum = arr.pop();
+//     let answer = LastNum;
+//     let count = 0;
+
+//     while(count < 3) {
+//         count = 0;
+//         for(let x of arr) {
+//             if(answer % x !== 0) {
+//                 answer += LastNum;
+//                 break;
+//             }
+//             count++;
+//         }
+//     }
+//     return answer;
+// }
+
+// let a = [2, 6, 8, 14];
+// console.log(solution(a));
